0ca0b2504cec51b35085ff0b2575b8208bc0543d
Ajay <ajaykumar84842@gamil.com>
2024-10-02 16:01:32 +0530

    clerk auth added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import vercel from '@astrojs/vercel/serverless';
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(),]
+  integrations: [tailwind(), compress(), clerk(),],
+  output: 'server',
+  adapter: vercel(),
 });
